21efcfde2999231f6de71e29b0a0d92956805bb1
Nicholas Land <land.d.nicholas@gmail.com>
2019-07-11 19:16:14 -0400

    Finished designing our interface chapter
    The app runs as it should

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Nicholas Land on 7/10/19.
+//  Copyright © 2019 Nicholas Land. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var pictures = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        // Do any additional setup after loading the view.
+        print(pictures)
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+
+}
+
