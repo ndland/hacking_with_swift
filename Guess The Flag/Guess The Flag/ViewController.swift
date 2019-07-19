9f933c5b731164d03b5179b9c1ddabe7c6ed299b
Nicholas Land <land.d.nicholas@gmail.com>
2019-07-19 19:08:13 -0400

    Finish Designing your layout chapter for Guess The Flag

---
+++ Guess The Flag/Guess The Flag/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Guess The Flag
+//
+//  Created by Nicholas Land on 7/19/19.
+//  Copyright © 2019 Nicholas Land. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var button1: UIView!
+    @IBOutlet var button2: UIView!
+    @IBOutlet var button3: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
